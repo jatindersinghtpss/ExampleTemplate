e3919e064832d58b400134e34f629f9b56f7a7c5
jatindersinghtpss <88339165+jatindersinghtpss@users.noreply.github.com>
2022-02-08 18:33:17 +0530

    index for updating
    new file

---
+++ index.php
@@ -0,0 +1,164 @@
+<?php include  'include/header.php' ?>
+
+ <!-- banner html start -->
+ <section class="banner-section">
+              <div class="container pos-relative">
+                  <div class="circleImg pos-absolute w-100">
+                        <img data-speed="-4" class="layer" src="./assets/images/circle01.png" alt="" style="transform: translateX(55.12px) translateY(18px);">
+                        <img data-speed="6" class="layer" src="./assets/images/circle02.png" alt="" style="transform: translateX(-34.68px) translateY(-17.5px);">
+                        <img data-speed="10" class="layer" src="./assets/images/circle03.png" alt="" style="transform: translateX(-70.6px) translateY(-31.7px);">
+                    </div>
+                  <div class="row align-items-center justify-content-between flex-direction-lg-column">
+                      <div class="col">
+                          <div class="banner-content text-lg-center">
+                            <span class="fw-bolder text-color-gray sub-heading">Hey,</span>
+                            <h1 class="fw-bolder m-0 text-color-primary">I’m <span class="primary-color-gradient">Lee</span><span class="secondary-color-gradient">mo</span></h1>
+                            <h2 class="fw-semi-bold text-color-primary">I digitise your business processes.</h2>
+                            <p>It is my name that defines my existence.</p>
+                            <p><span class="text-color-secondry"><b>Lee</b></span> Stand for <b>shelter</b> and <span class="text-color-blue-secondry"><b>MO</b></span> Stand for <b>modus operandi</b>.</p>
+                            <h2 class="childHeading text-color-primary fw-semi-bold mb-0">I am the head of the family.</h2>
+                        </div>  
+                    </div>
+                      <div class="col-auto">
+                          <div class="banner-img">
+                              <img src="./assets/images/buddy.png" alt="Lemmo">
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </section>
+          <!-- solutions wrapper html start -->
+          <section class="solution-wrap">
+              <div class="container">
+                  <span class="subheading text-color-primary fw-semi-bold text-lg-center d-inline-block">I help the companies <span class="text-color-gray">who use</span> freelancing portals to generate business..</span>
+                  <h2 class="section-heading-lg text-color-primary fw-bolder text-lg-center">Among my solutions is the <span class="text-color-secondry">freelancing bundle..</span></h2>    
+                  <div class="solution-list pos-relative text-lg-center">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 col-12">
+                            <div class="solution-img text-center">
+                                <img src="./assets/images/bitter-truth.png" alt="Bitter Truth">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <div class="solution-content m-a mt-lg-2">
+                                <h3 class="section-heading text-color-primary fw-bolder"><span class="text-color-gray">The</span> Bitter Truth</h3>
+                                <p>I saw people working in traditional methods. The majority of people have so many good things planned for their lives,
+but ultimately spend their time doing unproductive things. I've seen startup challenges, I've seen people management issues,  I have been a witness of policy breaching of freelancing channels by users like people sharing their
+own user account information which is never recommended by the freelancing portal. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row align-items-center flex-direction-row-reverse">
+                        <div class="col-lg-6 col-12 ">
+                            <div class="solution-img text-center">
+                                <img src="./assets/images/growth.png" alt="Bitter Truth">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-12 ">
+                            <div class="solution-content m-a mt-lg-2">
+                                <h3 class="section-heading text-color-primary fw-bolder"><span class="text-color-gray">The</span> Growth Prospect</h3>
+                                <p>However the freelancing portals mainly created for individual freelancers but it was adapted as a business model by many companies from around
+the world. The clients, businesses, companies, freelancers started growing in the difficult time of recession in 2008. 2009 and 2010.  I have experience of being ok to good, good to better, and the list is improving day by day.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 col-12">
+                            <div class="solution-img text-center">
+                                <img src="./assets/images/problem.png" alt="Bitter Truth">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <div class="solution-content m-a mt-lg-2">
+                                <h3 class="section-heading text-color-primary fw-bolder"><span class="text-color-gray">The</span> Problem Statement</h3>
+                                <p>It is not everyone's cup of tea to adopt freelancing as a business model. It needs a versatile approach. Whenever a prospect approaches you, he expects the same person as the profile, but 90% of the time he meets a business person from the company. So the major factor in a project, which is listening to the client's requirements, is compromised as companies need to explain the process, sometimes in a hidden way or sometimes in a real way. Most of the startups fail to convey this as they don’t have the practical ideas. If a company is delivery oriented, they may add non-billable resources to deliver the successful product to the client, which ultimately leads to loss for the organisation.</p>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+              </div>
+          </section>
+          <!-- solutions services html start -->
+          <section class="solution-services">
+              
+              <div class="container">
+                  <p class="subheading text-color-secondry-gray fw-medium text-lg-center">I educated my family so that everyone could be able to <b class="text-color-primary">deliver a particular solution for you.</b>
+<br> Our Solutions fall into three main areas. <span class="badges bg-secondry fw-semi-bold text-color-white br-5 d-inline-block">Management</span> 
+<span class="badges bg-blue-secondry fw-semi-bold text-color-white br-5 d-inline-block">Delivery</span> 
+<span class="badges bg-purple fw-semi-bold text-color-white br-5 d-inline-block">Sales</span></p>
+                  <h2 class="section-heading-lg fw-bolder text-color-primary text-lg-center"><span id="solution"></span> Our <span class="text-color-secondry">Solutions.</span></h2>
+                  <div class="services-list row text-lg-center">
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="cards ">
+                            <span class="card-img mb-2 d-inline-block">
+                                <a href="http://uproster.io/" target="_blank"><img src="./assets/images/uproster.png" alt="uproster"></a>
+                            </span>
+                            <h3 class="card-heading text-color-primary fw-semi-bold"><a href="http://uproster.io/"  target="_blank">Uproster</a></h3>
+                            <span class="badges-sm bg-secondry fw-semi-bold text-color-white br-5 d-inline-block">Management</span>
+                            <p class="card-info text-color-secondry-gray">Increase your Job invites/leads by optimizing your freelancer profiles as per the tested and latest approaches.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="cards">
+                            <span class="card-img mb-2 d-inline-block">
+                                <a href="http://leecage.io/"  target="_blank"><img src="./assets/images/leecage.png" alt="leecage"></a>
+                            </span>
+                            <h3 class="card-heading fw-semi-bold"><a href="http://leecage.io/"  target="_blank">Leecage</a></h3>
+                            <span class="badges-sm bg-blue-secondry fw-semi-bold text-color-white br-5 d-inline-block">Delivery</span>
+                            <p class="card-info text-color-secondry-gray">The perfect collaborative tool to work with Finance team and production team at same time. Identify the leakage in the processes.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="cards">
+                            <span class="card-img mb-2 d-inline-block">
+                                <a href="http://leeom.io/"  target="_blank"><img src="./assets/images/leeom.png" alt="leeom"></a>
+                            </span>
+                            <h3 class="card-heading text-color-primary fw-semi-bold"><a href="http://leeom.io/"  target="_blank">Leeom</a></h3>
+                            <span class="badges-sm bg-purple fw-semi-bold text-color-white br-5 d-inline-block">Sales</span>
+                            <p class="card-info text-color-secondry-gray">Opportunity management with the right CRM. Stay connected to your potential leads, pursue oppurtunities & convert them to recurring revenue.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="cards">
+                            <span class="card-img mb-2 d-inline-block">
+                                <a href="http://triggers.leecage.io"  target="_blank"><img src="./assets/images/triggers.png" alt="triggers"></a>
+                            </span>
+                            <h3 class="card-heading text-color-primary fw-semi-bold"><a href="http://triggers.leecage.io"  target="_blank">Triggers</a></h3>
+                            <span class="badges-sm bg-blue-secondry fw-semi-bold text-color-white br-5 d-inline-block">Delivery</span>
+                            <p class="card-info text-color-secondry-gray">Be proactive, not reactive. A market leading solution for software notifications triggers to keep you connected with the goals & delays.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="cards">
+                            <span class="card-img mb-2 d-inline-block">
+                               <a href="https://qb.leeom.io/"  target="_blank"><img src="./assets/images/builder.png" alt="builder"></a>
+                            </span>
+                            <h3 class="card-heading text-color-primary fw-semi-bold"><a href="https://qb.leeom.io/"  target="_blank">Quotation Builder</a></h3>
+                            <span class="badges-sm bg-purple fw-semi-bold text-color-white br-5 d-inline-block">Sales</span>
+                            <p class="card-info text-color-secondry-gray">Enlighten your prospective clients regarding the prices you offer for their software needs.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="invited">
+                            <img src="./assets/images/invited.jpg" alt="invited">
+                        </div>
+                    </div>
+                  </div>
+              </div>
+          </section>
+          <!-- video wrapper html start -->
+          <section class="leemo-video pos-relative">
+              <div class="container">
+                  <div class="video-wrapper pos-relative m-a overflow-hidden">
+                      <img src="./assets/images/video-wrap.png" alt="computer">
+                      <div class="video pos-absolute d-inline-block">
+                          <img src="./assets/images/video-cover.jpg" alt="video">
+                          <span class="video-trigger pos-absolute transform-middle-center ">
+                                <img src="./assets/images/video-icon.png" alt="video">
+                            </span>
+                      </div>
+                  </div>
+              </div>
+          </section>
+
+          <?php include  'include/footer.php' ?>
